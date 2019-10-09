9a00f0cc6494f4155835d613d0a6d45e0d060a5c
AnhMinhTran <AnhMinhTran@users.noreply.github.com>
2019-10-09 15:58:51 +1100

    Update video_proccessing.py
    Clean up code

--- video_proccessing.py
+++ video_proccessing.py
@@ -8,7 +8,6 @@ size = (
     int(capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
 )
 codec = cv2.VideoWriter_fourcc(*'DIVX')
-# output = cv2.VideoWriter('videofile_masked.avi', codec, 60.0, size)
 
 while(capture.isOpened()):
     ret, frame = capture.read()
@@ -19,12 +18,6 @@ while(capture.isOpened()):
         frame = display_instances(
             frame, r['rois'], r['masks'], r['class_ids'], class_names, r['scores']
         )
-        # results = model.detect([frame], verbose=0)
-        # r = results[0]
-        # frame = display_instances(
-        #     frame, r['rois'], r['masks'], r['class_ids'], class_names, r['scores']
-        # )
-        # output.write(frame)
         cv2.imshow('frame', frame)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
